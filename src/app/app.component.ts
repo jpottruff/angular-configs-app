7dd50243119400de34e3e8ce2caeb24af1942c89
Jeff Pottruff <jeffpottruff@gmail.com>
2023-04-01 09:30:58 -0400

    modify husk script so formatting is added after commit hook is ran (problematic version)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-configs-app';
+
+  fakeFun() {
+    this.title = ' alksjhdflkjasdkfjhalskdfhlkjasdhfkajlshfakjlshdfaskjdaf';
+
+    console.log('hi');
+    const thing = 1;
+
+    const stuff = 'hi';
+  }
 }
